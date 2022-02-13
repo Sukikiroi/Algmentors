04698b711c36a4eeff1883f2ebb0625030e00e42
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-13 14:56:45 +0100

    featurs: add routes private and public

---
+++ src/ router/privateRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const privateRoute = () => {
+  return (
+    <div>privateRoute</div>
+  )
+}
+
+export default privateRoute
---
+++ src/ router/publicRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const publicRoute = () => {
+  return (
+    <div>publicRoute</div>
+  )
+}
+
+export default publicRoute
